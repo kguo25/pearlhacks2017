729c77eaaf24d646a56e4ab717a9a4cfdeb1085d
Katherine Guo <Katherine Guo>
2017-02-12 05:14:21 -0500

    fox home added

--- EcoPet/app/src/main/java/compsci290/edu/duke/ecopet/FoxHomeActivity.java
+++ EcoPet/app/src/main/java/compsci290/edu/duke/ecopet/FoxHomeActivity.java
@@ -1,7 +1,12 @@
 package compsci290.edu.duke.ecopet;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Katherine on 2/11/2017.
@@ -11,6 +16,36 @@ public class FoxHomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fox_home);
+
+        SharedPreferences prefs = getSharedPreferences("Sections", MODE_PRIVATE);
+        int total = prefs.getInt("Total", 0);
+        RelativeLayout l = (RelativeLayout)findViewById(R.id.fox_home_id);
+        TextView t = (TextView)findViewById(R.id.fox_message);
+        if(total <= 15) {
+            launchHappyFox(l, t);
+        }
+        else {
+            launchSadFox();
+        }
     }
+
+    public void launchHappyFox(RelativeLayout l, TextView t) {
+
+        l.setBackground(getResources().getDrawable(R.drawable.fox_house));
+        t.setText("Thanks for being an eco-friend citizen! The fox is very happy.");
+    }
+
+    public void launchSadFox() {
+
+    }
+
+    public void improve(View v) {
+
+    }
+
+    public void updateScore(View v) {
+
+    }
+
 
 }
